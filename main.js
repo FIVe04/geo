9b81421c75da36ceab791b7bece350b1e6c9e628
FIVe04 <83167979+FIVe04@users.noreply.github.com>
2021-09-22 21:59:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ window.addEventListener('load', ()=> {
             const long = position.coords.longitude;
             const lat = position.coords.latitude;
             console.log(lat, long);
-            
+            const text = document.getElementById('h1');
+            const text2 = document.getElementById('h2');
+            text.innerText = lat;
+            text2.innerText = long;
         });
     }
 });
